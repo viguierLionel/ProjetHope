a643a74de05ad4486b51b0a3f2f987cbaa9d5185
Hugo <Hugo@DESKTOP-0O31EMR>
2019-12-05 16:02:47 +0100

    add variables -> Commandes.jv

--- src/main/java/model/Commande.java
+++ src/main/java/model/Commande.java
@@ -13,8 +13,8 @@ public class Commande {
     
     private int numero;
     private String client;
-    private;
-    private;
+    private String saisieLe;
+    private String envoyeeLe;
     private float port;
     private String destinataire;
     private String adresseLivraison;
@@ -23,5 +23,31 @@ public class Commande {
     private String codePostalLivrais;
     private String paysLivraison;
     private float remise;
+
+    public Commande(int numero, String client, String saisieLe, String envoyeeLe, float port, String destinataire, String adresseLivraison, String villeLivraison, String regionLivraison, String codePostalLivrais, String paysLivraison, float remise) {
+        this.numero = numero;
+        this.client = client;
+        this.saisieLe = saisieLe;
+        this.envoyeeLe = envoyeeLe;
+        this.port = port;
+        this.destinataire = destinataire;
+        this.adresseLivraison = adresseLivraison;
+        this.villeLivraison = villeLivraison;
+        this.regionLivraison = regionLivraison;
+        this.codePostalLivrais = codePostalLivrais;
+        this.paysLivraison = paysLivraison;
+        this.remise = remise;
+    }
+    
+    public boolean dteVerif(String dte){
+        boolean dteConforme=false;
+
+        if (dte.length()==10 && dte.charAt(4) =='-' && dte.charAt(7)=='-'){
+            dteConforme=true;
+        } else {
+            dteConforme=false;
+        }
+        return dteConforme;
+    }
     
 }
