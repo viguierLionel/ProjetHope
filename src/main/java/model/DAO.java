23e67d77af9a5ae313bf9ce5c65c2b4a6f2fba44
Hugo <Hugo@DESKTOP-0O31EMR>
2019-12-09 22:19:50 +0100

    add methodes -> DAO.jv 					|-> methodes pour catégorie test ajout fichierTest -> DAOTest : à completer !

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import javax.sql.DataSource;
@@ -45,6 +46,8 @@ public class DAO {
             
             return result;
         }
+        
+        //PRODUIT ****************************************************************************************************************************
         
         /**
          * permet d'avoir une liste de produits dont le nom possède le mot clé entré en argument
@@ -98,4 +101,95 @@ public class DAO {
             return result;
         }
 
+        //CATEGORIE ****************************************************************************************************************************
+        
+        /**
+         * permet de retourner toutes les categories de produits
+         * @return liste de categories
+         * @throws SQLException 
+         */
+        public List<Categorie> toutesCategories() throws SQLException {
+        List<Categorie> resultat = new ArrayList();
+        String sql = "SELECT * FROM CATEGORIE";
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(sql)) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int code = rs.getInt("CODE");
+                String libelle = rs.getString("LIBELLE");
+                String description = rs.getString("DESCRIPTION");
+                Categorie c = new Categorie(code, libelle, description);
+                resultat.add(c);
+            }
+        }
+        return resultat;
+        }
+        
+        /**
+         * permet d'avoir une categorie en fonction d'un code
+         * @param code
+         * @return une categorie
+         * @throws SQLException 
+         */
+        public Categorie categorie(int code) throws SQLException {
+            Categorie categorie = null;
+            String sql = "SELECT * FROM CATEGORIE WHERE CODE = ?";
+            try (Connection myConnection = myDataSource.getConnection();
+                    PreparedStatement statement = myConnection.prepareStatement(sql)) {
+                        statement.setInt(1, code); 
+                        try (ResultSet rs = statement.executeQuery()) {
+                            if (rs.next()) {
+                                String libelle = rs.getString("LIBELLE");
+                                String description = rs.getString("DESCRIPTION");
+                                categorie = new Categorie(code, libelle, description);
+                            }
+                        }
+            }
+            return categorie;
+        }
+
+        /**
+         * permet d'avoir le libelle d'une categorie en fonction d'un code
+         * @param code
+         * @return libelle de la categorie
+         * @throws SQLException 
+         */
+        public String libelleCategorie(int code) throws SQLException {
+            String libelle = null;
+            String sql = "SELECT * FROM CATEGORIE WHERE CODE = ?";
+            try (Connection myConnection = myDataSource.getConnection();
+                    PreparedStatement statement = myConnection.prepareStatement(sql)) {
+                        statement.setInt(1, code); 
+                        try (ResultSet rs = statement.executeQuery()) {
+                            if (rs.next()) {
+                                libelle = rs.getString("LIBELLE");
+                            }
+                        }
+            }
+            return libelle;
+        }
+
+        /**
+         * permet d'avoir la description d'une categorie en fonction d'un code
+         * @param code
+         * @return description de la categorie
+         * @throws SQLException 
+         */
+        public String descriptionCategorie(int code) throws SQLException {
+            String description = null;
+            String sql = "SELECT * FROM CATEGORIE WHERE CODE = ?";
+            try (Connection myConnection = myDataSource.getConnection();
+                    PreparedStatement statement = myConnection.prepareStatement(sql)) {
+                        statement.setInt(1, code); 
+                        try (ResultSet rs = statement.executeQuery()) {
+                            if (rs.next()) {
+                                description = rs.getString("DESCRIPTION");
+                            }
+                        }
+            }
+            return description;
+        }
+        
 }
+
+
